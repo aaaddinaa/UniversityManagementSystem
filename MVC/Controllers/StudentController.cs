b7632f77f7c7e3055607df85199dbd756e5716f8
Adina <adina.n@yahoo.com>
2022-04-07 12:03:15 +0300

    Modified

--- MVC/Controllers/StudentController.cs
+++ MVC/Controllers/StudentController.cs
@@ -2,9 +2,7 @@
 using MVC.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace MVC.Controllers
 {
@@ -18,6 +16,45 @@ namespace MVC.Controllers
             studentsList = response.Content.ReadAsAsync<IList<StudentView>>().Result;
 
             return View(studentsList);
+        }
+
+        public ActionResult AddOrEdit(int id)
+        {
+            if(id == 0)
+            {
+                return View(new StudentView());
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Student/" + id.ToString()).Result;
+
+                return View(response.Content.ReadAsAsync<StudentView>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(StudentView student)
+        {
+            if(student.ID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Student", student).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Student", student).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Student/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Delete Successfuly";
+
+            return RedirectToAction("Index");
         }
     }
 }
