74490c493ea0a5fae79aca8a0d2b165bc071356f
Adina <adina.n@yahoo.com>
2022-04-08 17:49:50 +0300

    Modified

---
+++ MVC/Controllers/CourseController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+
+
+namespace MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<CourseView> coursesList;
+
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Course").Result;
+            coursesList = response.Content.ReadAsAsync<IList<CourseView>>().Result;
+
+            return View(coursesList);
+        }
+
+        public ActionResult AddOrEdit(int id)
+        {
+            if (id == 0)
+            {
+                return View(new CourseView());
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Course/" + id.ToString()).Result;
+
+                return View(response.Content.ReadAsAsync<CourseView>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(CourseView course)
+        {
+            if (course.ID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Course", course).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Course", course).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Course/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Delete Successfuly";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/Controllers/CourseTeacherController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MVC.Controllers
+{
+    public class CourseTeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<CourseTeacherView> courseTeachersList;
+
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("CourseTeacher").Result;
+            courseTeachersList = response.Content.ReadAsAsync<IList<CourseTeacherView>>().Result;
+
+            return View(courseTeachersList);
+        }
+
+        public ActionResult AddOrEdit(int id)
+        {
+            if (id == 0)
+            {
+                return View(new CourseTeacherView());
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("CourseTeacher/" + id.ToString()).Result;
+
+                return View(response.Content.ReadAsAsync<CourseTeacherView>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(CourseTeacherView courseTeacher)
+        {
+            if (courseTeacher.ID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("CourseTeacher", courseTeacher).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("CourseTeacher", courseTeacher).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("CourseTeacher/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Delete Successfuly";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
